20587d253ea11e43e609bfc07da61cf9cc51fa5c
JGadd74 <jon-k-g94@hotmail.com>
2020-09-12 10:19:28 -0400

    First commit. v0.3

---
+++ OneClickDownloadsOrganizer/FileOrganizer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace OneClickDownloadsOrganizer
+{
+    class FileOrganizer
+    {
+
+        private static readonly string activeUser = Environment.UserName;
+
+        private static  readonly string Main = @"C:\Users\" + activeUser + @"\Downloads";
+        //private static string Other = System.IO.Path.Combine(Main, "Unknown type");
+
+
+        private static readonly ExtensionsKit Ekit = new ExtensionsKit();
+        private static readonly IEnumerable<string> Files = Directory.EnumerateFiles(Main);
+        private const int CategoryName = 0;
+        public static int InitialFileCount;
+        public static Status ProgressStatus = Status.Ready;
+
+        public void CreateDummieFiles(int count)
+        {
+            for(int i = 0; i<=count; i++)
+            {
+                string name = i.ToString() + ".txt";
+                File.Create(Path.Combine(Main, name));
+            }
+            InitialFileCount = Files.Count() - 1;
+        }
+
+        public delegate void FileCountUpdate(object source, EventArgs args);
+
+        public event FileCountUpdate FileCountUpdated;
+
+        protected virtual void OnFileCountUpdated()
+        {
+            FileCountUpdated?.Invoke(this, EventArgs.Empty);
+        }
+      
+        public int GetFileCount()
+        {
+            int cnt = Directory.EnumerateFiles(Main).Count();
+            return cnt - 1;
+        }
+        public void MonitorFileCount()
+        {
+            if (GetFileCount() != InitialFileCount) OnFileCountUpdated();
+            if (GetFileCount() == 0) 
+            {
+                OnFileCountUpdated();
+                //ProgressStatus = Status.Finished;
+            }
+        }
+
+        public delegate void OrganizeStart(object source, EventArgs args);
+        public event OrganizeStart OrganizeStarted;
+        protected virtual void OnOrganizeStarted()
+        {
+            OrganizeStarted?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool OrganizeDownloads()
+        {
+            OnOrganizeStarted();
+            InitialFileCount = Files.Count();
+            ProgressStatus = Status.Processing;
+          
+            foreach (string file in Files)
+            {
+                foreach (string[] category in Ekit.ExtensionCategories) 
+                {
+                    foreach (string extension in category)
+                    {
+                        if (System.IO.Path.GetExtension(file) == extension)
+                        {
+                            if (!Directory.Exists(System.IO.Path.Combine(Main, category[CategoryName])))
+                            {
+                                Directory.CreateDirectory(System.IO.Path.Combine(Main, category[CategoryName]));
+                            }
+                            string fileName = Path.GetFileName(file);
+                            File.Move(file, Path.Combine(Main, category[CategoryName], fileName));
+                            MonitorFileCount();
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        public enum Status
+        {
+            Ready,
+            Processing,
+            Finished
+        }
+
+
+
+
+    }
+}
---
+++ OneClickDownloadsOrganizer/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
+
+namespace OneClickDownloadsOrganizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Organizer.FileCountUpdated += Organizer_FileCountUpdated;
+            Organizer.OrganizeStarted += Organizer_OrganizeStarted;
+            Max = Organizer.GetFileCount();
+            
+            MyProgressBar.Value = 0;
+        }
+
+        private void Organizer_OrganizeStarted(object source, EventArgs args)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                if (Organizer.GetFileCount() == 0) MyProgressBar.Maximum = 1;
+                else 
+                {
+                    Max = Organizer.GetFileCount();
+                    MyProgressBar.Maximum = Organizer.GetFileCount();
+                }
+            });
+        }
+        int Max = 0;
+        readonly FileOrganizer Organizer = new FileOrganizer();
+
+        private void Organizer_FileCountUpdated(object source, EventArgs args)
+        {
+            UpdateProgressBar();
+        }
+        public void SayStatus()
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                Head.Text = "Files Remaining: " + Organizer.GetFileCount().ToString();
+                Thread.Sleep(0);
+            });
+        }
+        public void UpdateProgressBar()
+        {
+            int tmp = Max - Organizer.GetFileCount();
+            this.Dispatcher.Invoke(() =>
+            {
+                if (Organizer.GetFileCount() == 0) SayDone();
+                else SayStatus();
+                
+                MyProgressBar.Value = tmp;
+            });
+        }
+        bool AutoIsEnabled = false;
+
+        private void Button_Click_Organize(object sender, RoutedEventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem((O) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    AutoIsEnabled = AutoCheck.IsChecked.Value;
+                });
+
+                if (!AutoIsEnabled) Organizer.OrganizeDownloads();
+                {
+                    while(FileOrganizer.ProgressStatus != FileOrganizer.Status.Finished)
+                    {
+                        Organizer.OrganizeDownloads();
+                        Thread.Sleep(60000);//1 min
+                        if (!AutoIsEnabled) break;
+                    }
+                }
+            });       
+        }
+        
+        private void Button_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            FileOrganizer.ProgressStatus = FileOrganizer.Status.Finished;
+            System.Windows.Application.Current.Shutdown();
+        }
+
+       public void SayDone()
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                Head.Text = "Done!";
+            });
+        }
+
+        private void CreateDummyFiles_Click(object sender, RoutedEventArgs e)
+        {
+            Organizer.CreateDummieFiles(1000);
+        }
+
+        private void AutoCheck_Checked(object sender, RoutedEventArgs e)
+        {
+            AutoIsEnabled = true;
+        }
+
+        private void AutoCheck_Unchecked(object sender, RoutedEventArgs e)
+        {
+            AutoIsEnabled = false;
+        }
+    }
+}
